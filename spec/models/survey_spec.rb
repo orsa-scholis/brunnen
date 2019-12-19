9bc860d987b3bb5d4d740a9019197b8f4bad75ad
Philipp Fehr <philipp@thefehr.me>
2019-12-19 18:08:25 +0100

    Add nice root view (#33)
    * Add nice root view
    
    * Add specs for active scope
    
    * Fix rubocop
    
    * Update spec/models/survey_spec.rb
    
    Co-Authored-By: Lukas Bischof <me@luk4s.dev>
    
    * Fix tests

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -35,4 +35,28 @@ RSpec.describe Survey, type: :model do
       it { is_expected.to be true }
     end
   end
+
+  describe '#active' do
+    context 'when there is an active Survey' do
+      before do
+        create :survey, :inactive
+      end
+
+      let!(:active_survey) { create :survey }
+
+      it 'returns the active one' do
+        expect(described_class.active).to eq [active_survey]
+      end
+    end
+
+    context 'when there is no active Survey' do
+      before do
+        create :survey, :inactive
+      end
+
+      it 'returns the active one' do
+        expect(described_class.active).to be_empty
+      end
+    end
+  end
 end
