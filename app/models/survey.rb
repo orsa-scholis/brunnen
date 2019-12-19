0ab5c2e3fbb8369cad67548b429d2114304a28e6
Philipp Fehr <philipp@thefehr.me>
2019-12-19 16:42:41 +0100

    Add timeliness validation spec

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -5,6 +5,8 @@ class Survey < ApplicationRecord
   has_many :survey_entries, inverse_of: :survey, dependent: :destroy
 
   validates :active_from, :active_to, :title, presence: true
+  validates_datetime :active_from, :active_to
+  validates_datetime :active_to, :after => :active_from
 
   translates :title
   globalize_accessors
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -8,4 +8,27 @@ RSpec.describe Survey, type: :model do
   it { is_expected.to validate_presence_of :active_from }
   it { is_expected.to validate_presence_of :active_to }
   it { is_expected.to validate_presence_of :title }
+
+  describe '#active_to' do
+    subject { model.tap(&:validate).errors.added? :active_to, :before_active_from }
+
+    let(:active_from) { Time.zone.today.at_beginning_of_week }
+    let(:active_to) { active_from.at_end_of_week - 2.days }
+
+    context 'when active_to is after active_from' do
+      it { is_expected.to be false }
+    end
+
+    context 'when active_to is at active_from' do
+      let(:active_to) { active_from }
+
+      it { is_expected.to be false }
+    end
+
+    context 'when active_to is before active_from' do
+      let(:active_to) { active_from - 2.days }
+
+      it { is_expected.to be true }
+    end
+  end
 end
