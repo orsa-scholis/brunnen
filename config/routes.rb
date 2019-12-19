cd1466a2d2e62be26d8f85cc08ade7c1f3ca928b
Lukas Bischof <me@luk4s.dev>
2019-12-19 20:57:44 +0100

    Mount web UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root 'surveys#index'
 
   devise_for :administrators
+
+  authenticate :administrator do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   resources :surveys, only: :index do
     resources :survey_entries, only: %i[index create], as: 'entries'
