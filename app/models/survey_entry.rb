225317f1cdacd63183d2e674d70f6bef63dd7f09
Lukas Bischof <me@luk4s.dev>
2019-12-22 19:44:19 +0100

    Survey completeness validation

--- app/models/survey_entry.rb
+++ app/models/survey_entry.rb
@@ -5,6 +5,7 @@ class SurveyEntry < ApplicationRecord
   has_many :answers
 
   validates :survey, presence: true
+  validate :entry_completeness
 
   after_create_commit :broadcast_statistics
 
@@ -14,5 +15,19 @@ class SurveyEntry < ApplicationRecord
 
   def broadcast_statistics
     BroadcastSurveyStatsJob.perform_later survey
+  end
+
+  def entry_completeness
+    question_ids = survey.questions.pluck(:id)
+
+    return errors.add(:answers, :too_short, count: question_ids) if answers.count != question_ids.length
+
+    errors.add(:answers, :not_all_answered) unless all_questions_answered? question_ids
+  end
+
+  def all_questions_answered?(question_ids)
+    answered_question_ids = answers.pluck(:question_id)
+
+    (question_ids - answered_question_ids).empty?
   end
 end
