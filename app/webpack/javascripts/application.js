ed6ebc57f76642e03fd7b5626587429bf5f1f3ff
Lukas Bischof <me@luk4s.dev>
2020-01-02 20:51:38 +0100

    Add fontawesome

--- app/webpack/javascripts/application.js
+++ app/webpack/javascripts/application.js
@@ -4,5 +4,6 @@ require("@rails/activestorage").start();
 require("channels");
 
 require('../stylesheets/application.scss');
+require('@fortawesome/fontawesome-free/css/solid.css');
 require('jquery/dist/jquery');
 require('bootstrap/dist/js/bootstrap');
--- app/webpack/packs/administrate.js
+++ app/webpack/packs/administrate.js
@@ -1,5 +1,6 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
+require("@rails/ujs").start();
+require("turbolinks").start();
 
-require("../javascripts/administrate/index")
-require("../stylesheets/administrate/application.scss")
+require("../javascripts/administrate/index");
+require("../stylesheets/administrate/application.scss");
+require('@fortawesome/fontawesome-free/css/solid.css');
