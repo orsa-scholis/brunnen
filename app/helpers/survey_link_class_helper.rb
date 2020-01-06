cbcd6ef989d3592a84f75812fb2251476e146db3
Lukas Bischof <me@luk4s.dev>
2020-01-06 22:45:12 +0100

    Remove static call

--- app/helpers/survey_link_class_helper.rb
+++ app/helpers/survey_link_class_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SurveyLinkClassHelper
-  def self.link_classes(survey)
+  def link_classes(survey)
     classes = 'list-group-item list-group-item-action list-group-item-'
     classes += 'primary' if survey.active?
     classes += 'secondary' unless survey.active?
