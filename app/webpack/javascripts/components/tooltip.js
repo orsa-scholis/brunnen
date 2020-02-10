12ac519ee684127f01ec1c9fb34731952101f7f9
Lukas Bischof <me@luk4s.dev>
2020-02-10 22:15:40 +0100

    Optimize imports

--- app/webpack/javascripts/components/tooltip.js
+++ app/webpack/javascripts/components/tooltip.js
@@ -1,6 +1,4 @@
-require('../../stylesheets/application.scss');
-require('jquery/dist/jquery');
-require('bootstrap/dist/js/bootstrap.bundle');
+import 'bootstrap/dist/js/bootstrap.bundle';
 import * as $ from 'jquery'
 
 $(document).ready(() => $('[data-toggle="tooltip"]').tooltip());
