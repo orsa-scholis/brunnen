66a09eaef691a2445de072a571837e8e22081c96
Lukas Bischof <me@luk4s.dev>
2020-02-10 22:28:57 +0100

    Use devise authentication method

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,14 +5,14 @@ require 'concerns/submission_trackable'
 class SurveysController < ApplicationController
   include SubmissionTrackable
 
+  before_action :authenticate_administrator!, only: :qrcode
+
   def index
     @surveys = (administrator_signed_in? ? Survey.all : Survey.active).descending
   end
 
   def qrcode
     @survey = Survey.find(params[:id])
-    return redirect_to surveys_url unless administrator_signed_in?
-
     @qr_code = QrCodeService.new(@survey.short_url)
   end
 end
--- spec/requests/surveys_controllers_spec.rb
+++ spec/requests/surveys_controllers_spec.rb
@@ -14,20 +14,20 @@ RSpec.describe SurveysController, type: :request do
     let(:survey) { create :survey }
 
     context 'when there is no administrator signed in' do
+      before { get qrcode_survey_path(survey) }
+
       it 'redirects to surveys list' do
-        get qrcode_survey_path(survey)
-        expect(response).to redirect_to(surveys_url(locale: I18n.locale))
+        expect(response).to be_redirect
       end
     end
 
     context 'when there is an administrator signed in' do
-      before do
-        sign_in create(:administrator)
-      end
+      before { sign_in create(:administrator) }
 
-      it 'returns 200 status code' do
+      it 'returns 200 status code', :aggregate_failures do
         get qrcode_survey_path(survey)
         expect(response).to have_http_status(:ok)
+        expect(response.body).to include survey.title
       end
     end
   end
