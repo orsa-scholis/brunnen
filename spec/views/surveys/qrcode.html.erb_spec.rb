2f19ec1522ffd78ee84e8173c58781a3cd88dffc
Lukas Bischof <me@luk4s.dev>
2020-02-10 22:35:03 +0100

    Fix specs

--- spec/views/surveys/qrcode.html.erb_spec.rb
+++ spec/views/surveys/qrcode.html.erb_spec.rb
@@ -5,23 +5,22 @@ require 'rails_helper'
 RSpec.describe 'surveys/qrcode.html.erb', type: :view do
   subject { rendered }
 
-  let(:survey) { instance_double(Survey, id: 1, title: 'My nice survey', short_url: 'short url') }
+  let(:survey) { build_stubbed(:survey, id: 1, title: 'My nice survey', short_url: 'short url') }
   let(:qr_code) { instance_double(QrCodeService, to_svg: 'hi im the svg') }
 
   before do
     assign(:survey, survey)
     assign(:qr_code, qr_code)
+    render
   end
 
-  context 'when administrator is signed in' do
-    before do
-      sign_in create(:administrator)
-      render
-    end
+  it { is_expected.to include survey.title }
+  it { is_expected.to include survey.short_url }
+  it { is_expected.to include qr_code.to_svg }
 
-    it { is_expected.to include survey.title }
-    it { is_expected.to include survey.short_url }
-    it { is_expected.to include qr_code.to_svg }
-    it { is_expected.to include I18n.t('surveys.qrcode.scan_explanation') }
+  it 'has all steps', :aggregate_failures do
+    I18n.t('surveys.qrcode.scan_explanation').each do |step|
+      expect(rendered).to have_content step
+    end
   end
 end
