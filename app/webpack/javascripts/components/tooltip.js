25b2e4f6a50da4f8d4d4c55f2b3b91371b9bd693
Lukas Bischof <me@luk4s.dev>
2020-02-22 15:21:05 +0100

    Fix Tooltips (#112)
    * Fix tooltips
    
    * Fix deprecation warning

--- app/webpack/javascripts/components/tooltip.js
+++ app/webpack/javascripts/components/tooltip.js
@@ -1,4 +1,10 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 import * as $ from 'jquery'
 
-$(document).ready(() => $('[data-toggle="tooltip"]').tooltip());
+const setupTooltips = () => $('[data-toggle="tooltip"]').tooltip();
+
+if ('Turbolinks' in window && Turbolinks.supported) {
+  document.addEventListener('turbolinks:load', setupTooltips);
+} else {
+  $(document).ready(setupTooltips);
+}
