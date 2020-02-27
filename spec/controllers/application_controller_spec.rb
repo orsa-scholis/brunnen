ba454eb502fba6032cf54ab7467a8bb2b9db714f
Lukas Bischof <me@luk4s.dev>
2020-02-27 10:37:12 +0100

    Provide more Sentry context (#113)
    * Sentry context
    
    * SPEECCCCCCCCCCCCCCKKKS

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -39,4 +39,27 @@ RSpec.describe ApplicationController, type: :controller do
       end
     end
   end
+
+  describe '#set_raven_context' do
+    before { allow(Raven).to receive(:extra_context) }
+
+    it 'does not call raven api' do
+      get :index
+      expect(Raven).not_to have_received :extra_context
+    end
+
+    context 'when rails environment is production' do
+      before do
+        allow(Rails).to receive(:env).and_return('production'.inquiry)
+        get :index
+      end
+
+      it 'provides extra_context to Raven' do
+        expect(Raven).to have_received(:extra_context).with(
+          hash_including(params: be_a_kind_of(Hash),
+                         url: be_a(String))
+        )
+      end
+    end
+  end
 end
