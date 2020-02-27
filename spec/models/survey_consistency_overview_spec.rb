ab64f401162654f35eadda0023c8cca6830110b1
Lukas Bischof <me@luk4s.dev>
2020-02-27 20:55:23 +0100

    Consistency overview spec

--- spec/models/survey_consistency_overview_spec.rb
+++ spec/models/survey_consistency_overview_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe SurveyConsistencyOverview, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :survey }
+  it { is_expected.to belong_to :question_group }
+  it { is_expected.to belong_to :question }
+
+  it 'is a readonly model' do
+    expect(described_class.new.readonly?).to eq true
+  end
 end
